015717689e3687c80db980558ac7d9627a833ec0
SaraCS20 <21sara.alsaifi@boiseschools.net>
2020-08-20 18:18:26 +0000

    Finished the assignment

---
+++ Lab1HelloWorldCode.java
@@ -0,0 +1,15 @@
+//Sara Alsaifi
+//AP CS A Period 2
+//20 August 2020
+
+public class Lab1HelloWorldCode{
+  public static void main(String args[]){
+    /*
+    This is a goofy picture   
+    */ 
+    System.out.println( "{-_-}");
+    System.out.println("--l---");
+    System.out.println(" //");
+    System.out.println("< >");
+  }
+}
